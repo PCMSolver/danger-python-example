61d6b67e621d32ea93566bd0ce9442bab89f5078
Roberto Di Remigio <robertodr@users.noreply.github.com>
2017-09-30 18:51:24 +0200

    Set up CI and Danger integration

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,15 @@
+def vector(dim):
+    import numpy as np
+
+
+    return np.random.rand(dim)
+
 def message():
     return 'Hello, Danger world!'
 
 def main():
     print(message())
+    print(vector(10))
 
 if __name__ == '__main__':
     main()
